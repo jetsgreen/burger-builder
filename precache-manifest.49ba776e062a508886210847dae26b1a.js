d695214a1e9399a353a3c5e596d0ed1b3e07b92a
Felix Fernandez <fernandezfelix460.gmail.com>
2020-04-01 10:28:56 -0400

    Updates

---
+++ precache-manifest.49ba776e062a508886210847dae26b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215b052489a46785ba9c70d9e9e61721",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "f3bc09d7e22ebcce7464",
+    "url": "/burger-builder/static/css/main.77413427.chunk.css"
+  },
+  {
+    "revision": "d6b8264b8f5785426748",
+    "url": "/burger-builder/static/js/2.52279300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.52279300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bc09d7e22ebcce7464",
+    "url": "/burger-builder/static/js/main.4cb0dc1c.chunk.js"
+  },
+  {
+    "revision": "583042396e8cc56c1062",
+    "url": "/burger-builder/static/js/runtime-main.2a419647.js"
+  }
+]);
